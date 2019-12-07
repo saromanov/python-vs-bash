8f949491aaa1cb30c192628dd4c13a6d799e77da
Sergey <xxsmotur@gmail.com>
2019-12-08 00:20:10 +0500

    Add bash method for check if user is exist

--- commands-executing/commands.sh
+++ commands-executing/commands.sh
@@ -1,2 +1,20 @@
-w
-awk -F':' '{ print $1}' /etc/passwd
+#!/bin/bash
+
+get_users()
+{
+    awk -F':' '{ print $1}' /etc/passwd
+}
+
+check_exist_user()
+{
+    for data in $(awk -F':' '{ print $1}' /etc/passwd)
+    do
+        if [[ $data == $1 ]]; then
+            echo "TRUE"
+            return
+        fi
+    done
+    echo "FALSE"
+}
+
+check_exist_user $1

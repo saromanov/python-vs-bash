bce8debac1112453d60b47015b3b7ecd8c283f9b
Sergey <xxsmotur@gmail.com>
2019-12-07 23:57:38 +0500

    other way for read file

--- commands-executing/commands.py
+++ commands-executing/commands.py
@@ -7,4 +7,9 @@ def execute_list_of_users():
     cmd = "awk -F':' '{print $1}' /etc/passwd"
     subprocess.run(cmd.split())
 
-execute_list_of_users()
+def execute_list_of_users_read():
+    data = open('/etc/passwd')
+    print(list(filter(lambda x: x.split()[0], data.readlines())))
+    data.close()
+
+execute_list_of_users_read()
